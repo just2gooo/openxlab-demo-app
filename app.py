332c2a8d12ec602eb8bf31deb354a5069c61bc74
just2gooo <7363124+just2gooo@users.noreply.github.com>
2024-07-16 17:35:47 +0800

    使用本地文件作为labels

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import gradio as gr
 import torch
 import requests
+import os
 from torchvision import transforms
 
 torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
 model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True).eval()
-response = requests.get("https://git.io/JJkYN")
-labels = response.text.split("\n")
+# response = requests.get("https://git.io/JJkYN")
+
+# 获取当前文件的目录
+current_directory = os.path.dirname(os.path.abspath(__file__))
+with open(f'{current_directory}/labels.txt', 'r', encoding='utf-8') as file:
+  # 读取文件内容
+  content = file.read()
+  labels = content.split("\n")
 
 def predict(inp):
   inp = transforms.ToTensor()(inp).unsqueeze(0)
